e1a1b74bbc42e8c2ee3f43eb75ae0f122432ea52
abdelazizade <abdelaziz.adel.m@gmail.com>
2026-03-01 21:10:51 +0200

    [fix] test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'ci-cd' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).not.toEqual('ci-cd');
+    expect(app.title).toEqual('ci-cd');
   });
 
   it('should render title', () => {
