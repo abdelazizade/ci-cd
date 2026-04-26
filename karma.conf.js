169b0749493fe8cdffa9add4768bfab43d6516f4
abdelazizade <abdelaziz.adel.m@gmail.com>
2026-04-26 08:01:11 +0300

    change node versian

---
+++ karma.conf.js
@@ -0,0 +1,16 @@
+module.exports = function (config) {
+    config.set({
+        browsers: ['ChromeHeadlessCI'],
+
+        customLaunchers: {
+            ChromeHeadlessCI: {
+                base: 'ChromeHeadless',
+                flags: [
+                    '--no-sandbox',
+                    '--disable-gpu',
+                    '--disable-dev-shm-usage'
+                ]
+            }
+        }
+    });
+};
