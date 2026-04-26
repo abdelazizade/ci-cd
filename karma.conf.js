aa06b59952ccd2570ff4ab6a4d377891a6766657
abdelazizade <abdelaziz.adel.m@gmail.com>
2026-04-26 08:13:45 +0300

    handle karma file

--- karma.conf.js
+++ karma.conf.js
@@ -1,18 +1,39 @@
 module.exports = function (config) {
     config.set({
-        // ... your existing config ...
+        basePath: '',
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage'),
+            require('@angular-devkit/build-angular/plugins/karma'),
+        ],
+        client: {
+            jasmine: {},
+            clearContext: false,
+        },
+        coverageReporter: {
+            dir: require('path').join(__dirname, './coverage/ci-cd'),
+            subdir: '.',
+            reporters: [{ type: 'html' }, { type: 'text-summary' }],
+        },
+        reporters: ['progress', 'kjhtml'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
 
-        browsers: ['ChromeHeadlessNoSandbox'],  // Change this line
-
+        // ✅ These two must match exactly:
+        browsers: ['ChromeHeadlessNoSandbox'],
         customLaunchers: {
             ChromeHeadlessNoSandbox: {
                 base: 'ChromeHeadless',
-                flags: [
-                    '--no-sandbox',
-                    '--disable-gpu',
-                    '--disable-dev-shm-usage'
-                ]
-            }
+                flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
+            },
         },
+
+        singleRun: false,
+        restartOnFileChange: true,
     });
 };
